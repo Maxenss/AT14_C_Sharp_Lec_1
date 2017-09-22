633acd794fd0a0b3e642cc3bfbce88360a20a78e
Maxenss <maxenssss@gmail.com>
2017-09-22 07:57:41 +0300

    Second commit
    Second commit

---
+++ Catv1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Lec_1
+{
+    class Catv1
+    {
+        // Поля класса
+        public string gender;
+        public string name;
+        public string color;
+        public int age;
+        public int countOfLifes;
+    }
+}
---
+++ Catv2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Lec_1
+{
+    class Catv2
+    {
+        // Поля класса
+        public string gender;
+        public string name;
+        public string color;
+        public int age;
+        public int countOfLifes;
+
+        // Пользовательский конструктор
+        public Catv2(string _gender, 
+            string _name, 
+            string _color, 
+            int _age, 
+            int _countOfLifes) {
+
+            gender = _gender;
+            name = _name;
+            age = _age;
+            color = _color;
+            countOfLifes = _countOfLifes;
+        }
+
+        // Конструктор по-умолчанию
+        public Catv2() {
+
+        }
+    }
+}
